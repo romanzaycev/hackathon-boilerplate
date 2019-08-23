13bc0d34dae04927c62b6bcf62a095b62e06fc51
Roman Zaycev <box@romanzaycev.ru>
2019-08-24 02:10:24 +0700

    Dev config via cli params, resolve #2

--- start-dev.sh
+++ start-dev.sh
@@ -5,4 +5,4 @@ if [ ! -d "./logs" ]; then
 fi
 
 DT=$(date "+%d_%m_%Y")
-nohup ./rr serve -v -d -l plain >"logs/stdout_${DT}.log" 2>"logs/stderr_${DT}.log" &
+nohup ./rr serve -v -d -l plain -o env.ENVIRONMENT="development" -o http.workers.pool.numWorkers=1 -o http.workers.pool.maxJobs=1 >"logs/stdout_${DT}.log" 2>"logs/stderr_${DT}.log" &
